0d2c16f2b282fcf585323ccb45c3319ff242fd28
summerGancf <summer.gan@LMXMN204.local>
2018-11-17 16:02:22 +0800

    mod case

---
+++ src/__test__/login.test.2.js
@@ -0,0 +1,44 @@
+const timeout = 100000
+
+describe(
+  '/ (Login Page)',
+  () => {
+    beforeAll(async () => {
+      await global.page.goto('http://localhost:8080')
+      const page = global.page;
+    }, timeout)
+
+    afterAll(async () => {
+      await driver.close(page)
+    })
+
+    it('should setting', async () => {
+      await page.evaluate('toggleEnv()');
+      await page.waitFor(1000);
+  
+      await page.click('[data-sign="envToggle"]');
+      await page.focus('[data-sign="envServerUrl"]');
+      await page.$eval('[data-sign="envServerUrl"]', input => input.select(), '[data-sign="envServerUrl"]');
+      await page.evaluate(() => {
+          document.dispatchEvent(new KeyboardEvent('keydown', { key: 'Delete' }));
+          document.dispatchEvent(new KeyboardEvent('keyup', { key: 'Delete' }));
+        });    
+      await page.type('[data-sign="envServerUrl"]', 'https://api-xmnup.lab.nordigy.ru');
+      await page.click('[data-sign="envSave"]');
+      await page.waitFor(1000);
+      await page.click('[data-sign="loginButton"]');
+      await page.waitFor(5000);
+        // swith to activity page
+      const allpages = await browser.pages();
+      const activityPage = allpages[allpages.length - 1];
+        //input longin name
+      await activityPage.type('[data-test-automation-id="input"]', "448451030241");
+      await activityPage.click( '[data-test-automation-id="loginCredentialNext"]');
+        //input password click signin
+      await activityPage.waitFor(3000);
+      await activityPage.type('[id=password]', 'Test!123');
+      await activityPage.click('[data-test-automation-id="signInBtn"]')
+      
+      await page.screenshot({path: 'screenshot.png'});
+    },timeout)
+  })
